a1696a8a1dba51bccafb54100257e2f1977e55fd
jernmon <jernmon1@hotmail.com>
2013-02-24 23:56:52 -0500

    Now my consumer credentials aren't in the source

--- main.py
+++ main.py
@@ -2,14 +2,17 @@ from twitter import *
 import os
 
 MY_TWITTER_CREDS = os.path.expanduser('~/.my_app_credentials')
+# Make it secure for each user
+CONSUMER_CREDS = [line.strip() for line in open(os.path.expanduser('~/.consumer_creds'))]
 if not os.path.exists(MY_TWITTER_CREDS):
-    oauth_dance("jernPi", "N4rUOi6kTQjTmRdB2Tg", "m8T2mTpRVJtsfp8GxnWl5tuf8xTZUg2S0DoWhqubPzI",
+    oauth_dance("jernPi", CONSUMER_CREDS[0], CONSUMER_CREDS[1],
                 MY_TWITTER_CREDS)
 
 oauth_token, oauth_secret = read_token_file(MY_TWITTER_CREDS)
 
 t = Twitter(auth=OAuth(
-    oauth_token, oauth_secret, "N4rUOi6kTQjTmRdB2Tg", "m8T2mTpRVJtsfp8GxnWl5tuf8xTZUg2S0DoWhqubPzI"))
+    oauth_token, oauth_secret, CONSUMER_CREDS[0], CONSUMER_CREDS[1]))
 
 # Updates your status
-t.statuses.update(status="Found out about @sixohsix's sweet Python Twitter Tools. I'm I doing this right?")
+# Taken out (for now)
+t.statuses.update(status=str(input()))
