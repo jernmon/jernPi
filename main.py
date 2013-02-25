dce867a4fa329dd7e3c9ec6a40da59f9c43d61f0
jernmon <jernmon1@hotmail.com>
2013-02-24 23:09:59 -0500

    Got Python Twitter Tools working

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print "Hello World"
+from twitter import *
+import os
+
+MY_TWITTER_CREDS = os.path.expanduser('~/.my_app_credentials')
+if not os.path.exists(MY_TWITTER_CREDS):
+    oauth_dance("jernPi", "N4rUOi6kTQjTmRdB2Tg", "m8T2mTpRVJtsfp8GxnWl5tuf8xTZUg2S0DoWhqubPzI",
+                MY_TWITTER_CREDS)
+
+oauth_token, oauth_secret = read_token_file(MY_TWITTER_CREDS)
+
+t = Twitter(auth=OAuth(
+    oauth_token, oauth_secret, "N4rUOi6kTQjTmRdB2Tg", "m8T2mTpRVJtsfp8GxnWl5tuf8xTZUg2S0DoWhqubPzI"))
+
+# Updates your status
+t.statuses.update(status="Found out about @sixohsix's sweet Python Twitter Tools. I'm I doing this right?")
